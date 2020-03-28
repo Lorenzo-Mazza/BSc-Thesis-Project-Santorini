cf14a54147f7caac0e899afa4313af924be88395
DinazRed <Albino20@hotmail.it>
2020-03-28 21:34:47 +0100

    Initial commit

---
+++ src/main/java/it/polimi/ingsw/PSP50/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.PSP50;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
